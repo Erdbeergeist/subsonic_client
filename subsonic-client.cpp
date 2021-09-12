6e96c5fb6c95dc22551c6603ed28649beae31cb0
Lukas Mittnacht <luxmitges@gmail.com>
2021-09-12 16:01:13 +0200

    removed useless auth info

--- subsonic-client.cpp
+++ subsonic-client.cpp
@@ -3,7 +3,7 @@
 
 int main(int argc, char *argv[]) {
 
-    subsonicAPI sAPI("https://192.168.1.155/index.php/apps/music/subsonic/rest/", "luxmit", "wpkuqfc3m17o", "subsonic-dev");
+    subsonicAPI sAPI("https://192.168.1.155/index.php/apps/music/subsonic/rest/", "", "", "subsonic-dev");
 
     std::cout<<sAPI.getMusicFolders()<<std::endl;
 
